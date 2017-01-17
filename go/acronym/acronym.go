a54fb71bcae6ec2d7485248c4e5f21842bb203e5
Craig Andrews <craig@lazyrobot.org.uk>
2017-01-17 23:39:53 +0000

    Add acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,19 @@
+package acronym
+
+import "unicode"
+
+const testVersion = 2
+
+// Abbreviate a string to its acronym.
+func Abbreviate(s string) string {
+	var acronym []rune
+	runes := []rune(s)
+	var last rune
+	for _, r := range runes {
+		if (unicode.IsUpper(r) && !unicode.IsUpper(last)) || (unicode.IsLetter(r) && !unicode.IsLetter(last)) {
+			acronym = append(acronym, unicode.ToUpper(r))
+		}
+		last = r
+	}
+	return string(acronym)
+}
