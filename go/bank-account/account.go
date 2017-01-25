ceac22f7f1aa507f4f90a3175deeb81b8dff94d9
Craig Andrews <craig@lazyrobot.org.uk>
2017-01-25 23:26:03 +0000

    DRY the bank account

--- go/bank-account/account.go
+++ go/bank-account/account.go
@@ -10,6 +10,8 @@ type Account struct {
 	balance int64
 }
 
+type adjustment func() bool
+
 // Open a new account
 func Open(initalDeposit int64) *Account {
 	if initalDeposit < 0 {
@@ -20,8 +22,8 @@ func Open(initalDeposit int64) *Account {
 	}
 }
 
-// Close the account
-func (a *Account) Close() (payout int64, ok bool) {
+// access the account and optionally make an adjustment
+func (a *Account) access(fn adjustment) (balance int64, ok bool) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
 
@@ -29,35 +31,34 @@ func (a *Account) Close() (payout int64, ok bool) {
 		return
 	}
 
-	a.closed = true
+	if fn != nil && !fn() {
+		return
+	}
+
 	return a.balance, true
+}
+
+// Close the account
+func (a *Account) Close() (payout int64, ok bool) {
+	return a.access(func() bool {
+		a.closed = true
+		return true
+	})
 }
 
 // Balance of the account
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.mutex.Lock()
-	defer a.mutex.Unlock()
-
-	if a.closed {
-		return
-	}
-
-	return a.balance, true
+	return a.access(nil)
 }
 
 // Deposit or withdraw from the account
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	a.mutex.Lock()
-	defer a.mutex.Unlock()
+	return a.access(func() bool {
+		if amount < 0 && a.balance+amount < 0 {
+			return false
+		}
 
-	if a.closed {
-		return
-	}
-
-	if amount < 0 && a.balance+amount < 0 {
-		return
-	}
-
-	a.balance += amount
-	return a.balance, true
+		a.balance += amount
+		return true
+	})
 }
