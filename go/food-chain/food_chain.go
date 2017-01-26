915f7c99a7874793aa7ee0a2a79f5df8490aee88
Craig Andrews <craig@lazyrobot.org.uk>
2017-01-26 11:23:13 +0000

    Eating the food chain

---
+++ go/food-chain/food_chain.go
@@ -0,0 +1,72 @@
+package foodchain
+
+import (
+	"fmt"
+	"strings"
+)
+
+var testVersion = 3
+
+// FoodChain describes the verse structure
+type FoodChain struct {
+	name        string
+	commentary  string
+	description string
+}
+
+var foodChain = []FoodChain{
+	{name: "fly"},
+	{name: "spider", commentary: "It wriggled and jiggled and tickled inside her.", description: "that wriggled and jiggled and tickled inside her"},
+	{name: "bird", commentary: "How absurd to swallow a bird!"},
+	{name: "cat", commentary: "Imagine that, to swallow a cat!"},
+	{name: "dog", commentary: "What a hog, to swallow a dog!"},
+	{name: "goat", commentary: "Just opened her throat and swallowed a goat!"},
+	{name: "cow", commentary: "I don't know how she swallowed a cow!"},
+	{name: "horse", commentary: "She's dead, of course!"},
+}
+
+// Verse returns one verse
+func Verse(num int) string {
+	var lines []string
+	food := foodChain[num-1]
+
+	lines = append(lines, fmt.Sprintf("I know an old lady who swallowed a %s.", food.name))
+
+	if food.commentary != "" {
+		lines = append(lines, food.commentary)
+	}
+
+	if num < len(foodChain) {
+		for l := num - 2; l >= 0; l-- {
+			food = foodChain[l+1]
+			prevFood := foodChain[l]
+
+			var description = ""
+			if prevFood.description != "" {
+				description = " " + prevFood.description
+			}
+
+			line := fmt.Sprintf("She swallowed the %s to catch the %s%s.", food.name, prevFood.name, description)
+			lines = append(lines, line)
+		}
+
+		lines = append(lines, "I don't know why she swallowed the fly. Perhaps she'll die.")
+	}
+
+	verse := strings.Join(lines, "\n")
+	return verse
+}
+
+// Verses returns a range of verses
+func Verses(start, end int) string {
+	var verses []string
+	for x := start; x <= end; x++ {
+		verses = append(verses, Verse(x))
+	}
+	return strings.Join(verses, "\n\n")
+}
+
+// Song returns the whole song
+func Song() string {
+	return Verses(1, len(foodChain))
+}
