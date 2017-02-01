2bf2e47124416367bc05a4b771d11aed882eb519
Craig Andrews <craig@lazyrobot.org.uk>
2017-02-01 14:12:42 +0000

    Transpose proper

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -1,14 +1,66 @@
 // Package transpose calculates the transpose of nested arrays
 package transpose
 
+func maxLength(input []string) int {
+	maxLen := 0
+	for ix := range input {
+		l := len(input[ix])
+		if l > maxLen {
+			maxLen = l
+		}
+	}
+	return maxLen
+}
+
+func makeRuneArrays(input []string) (runes [][]rune) {
+	for _, s := range input {
+		runes = append(runes, []rune(s))
+	}
+	return
+}
+
 // Transpose a nested array of strings
 //
-// Hilariously, this implementation passes all the tests. This is
-// because a zero-length result breaks the "clever" error finding
-// code in the test. It determines that the min length is 0 and
-// doesn't actually check any rows.
+// Works by making slices through the input arrays at
+// each "row" position in reverse column order and
+// working out if there should be a rune, a space, or
+// nothing at each position.
 //
-// I'll do an actual implementation now, promise.
+// e.g. taking this slice:
+//
+// ab|c|d
+// ef|g|h
+// ij
+//
+// produces the output string "cg". There is no padding
+// because g is the last valid char on the line.
 func Transpose(input []string) (transpose []string) {
+	rows := maxLength(input)
+	columns := len(input)
+	runes := makeRuneArrays(input)
+
+	for r := 0; r < rows; r++ {
+		pad := false
+		row := make([]rune, columns)
+
+		// Iterate over columns backwards to aid padding
+		for c := columns - 1; c >= 0; c-- {
+			switch {
+			case r < len(runes[c]):
+				// If there is a rune at this position, add it
+				row[c] = runes[c][r]
+				pad = true
+			case pad:
+				// otherwise, if a rune has been written, pad the column
+				row[c] = ' '
+			default:
+				// or if no rune written yet, remove the column entirely
+				row = row[:c]
+			}
+		}
+
+		transpose = append(transpose, string(row))
+	}
+
 	return
 }
