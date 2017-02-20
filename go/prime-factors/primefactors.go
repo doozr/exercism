f43b69c93d06016b5ccc8e730cffaeca8cfec1c1
Craig Andrews <craig@lazyrobot.org.uk>
2017-02-20 12:50:00 +0000

    Massive performance improvement on prime factors courtesy of markwest1

--- go/prime-factors/primefactors.go
+++ go/prime-factors/primefactors.go
@@ -6,7 +6,17 @@ var testVersion = 2
 // Factors returns the prime factors of n
 func Factors(n int64) []int64 {
 	factors := make([]int64, 0)
-	for i := int64(2); i*i <= n; i++ {
+
+	// Handle 2 (the only even prime) separately so subsequent
+	// checks can increment by 2
+	// Borrowed from markwest1's submission:
+	// http://exercism.io/submissions/0128f00f48e3400780d828cb7d0c5b25
+	for n%2 == 0 {
+		n = n / 2
+		factors = append(factors, 2)
+	}
+
+	for i := int64(3); i*i <= n; i += 2 {
 		for n%i == 0 {
 			n = n / i
 			factors = append(factors, i)
